7ea76c3441876e0c07389a70e84e3f097df38c4c
gitjisu <kimvely0422@gmail.com>
2022-06-24 03:06:16 +0900

    0624

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <div v-if="isLoggedIn" >
-    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-      <li>
+  <div>
+    <nav v-if="isLoggedIn" class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+      <ul>
         <a class="navbar-brand" href="#">Navbar</a>
-      </li>
+      </ul>
 
-      <li>
+      <ul>
         <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="검색" id="search-var">
+          <!-- <button class="btn btn-outline-primary" type="submit">Search</button> -->
         </form>
-      </li>
-      <li>
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-      </li>
-
-
+      </ul>
+      <ul>
+        <li>
+          <router-link :to="{ name: 'onestargram'}"><img v-if="isHome" src="@/assets/home2.png" alt="" id="home1"></router-link>
+          <router-link :to="{ name: 'onestargram'}"><img v-if="!isHome" src="@/assets/home2.png" alt="" id="home1"></router-link>
+        </li>
+        <li>
+          <a class="nav-link active" aria-current="page" href="#">Login</a>
+        </li>
+      </ul>
     </nav>
     <router-view/>
   </div>
@@ -29,8 +31,21 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'App',
   computed: {
-    ...mapGetters(['isLoggedIn'])
+    ...mapGetters(['isLoggedIn']),
+  },
+  data() {
+    return {
+      isHome: false,
+    }
+  },
+  created() {
+    let now = window.location.href
+    console.log(now)
+    if (now === "http://localhost:8080/") {
+      this.isHome = true
+    }
   }
+
 }
 </script>
 
@@ -40,5 +55,19 @@ export default {
 #search-var {
   width: 300px;
   background-color: rgb(228, 228, 228);
+}
+
+ul {
+  list-style: none;
+}
+
+li {
+  float: left;
+}
+
+#home1 {
+  margin-top: 10px;
+  width: 24px;
+  height: 24px;
 }
 </style>
--- front/src/views/OneStarGramView.vue
+++ front/src/views/OneStarGramView.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <!-- 로그인 되어 있지 않을경우 최초 페이지 렌더링 -->
+
+    <div v-if="isLoggedIn">
+      <main-page></main-page>
+    </div>
+
     <div v-if="!isLoggedIn">
       <first-page></first-page>
     </div>
 
-    <!-- 로그인 되어 있을 경우 게시글 목록 페이지 렌더링 -->
-    <div v-if="isLoggedIn">
-      <main-page></main-page>
-    </div>
+    <router-view/>
   </div>
 </template>
 
@@ -27,6 +28,9 @@ export default {
   },
   computed: {
     ...mapGetters(['isLoggedIn'])
+  },
+  mounted() {
+    console.log(1)
   }
 }
 </script>
